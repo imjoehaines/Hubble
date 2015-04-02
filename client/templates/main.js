ca94ec5ba6fe66dfad6d4463f7464209e14c93fb
Joe Haines <hello@joehaines.co.uk>
2015-04-02 14:14:36 +0100

    Add new branch button works
    Added getStatusName helper

--- client/templates/main.js
+++ client/templates/main.js
@@ -1,3 +1,19 @@
 Template.registerHelper("prettifyDate", function(timestamp) {
     return new Date(timestamp).toISOString().slice(0, 10);
 });
+
+Template.registerHelper("getStatusName", function(status) {
+    var statuses = {
+        created: 'Created',
+        inProgress: 'In Progress',
+        readyForReview: 'Ready for Review',
+        inReview: 'In Review',
+        prepareForMaster: 'Prepare for Master',
+        readyForMaster: 'Ready for Master',
+        mergedToMaster: 'Merged to Master',
+        deployed: 'Deployed',
+        deprecated: 'Deprecated'
+    };
+
+    return statuses[status];
+});
