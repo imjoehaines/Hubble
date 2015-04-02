66c2cdf91dcc101a832a0626a27ce52ff2e03181
Joe Haines <hello@joehaines.co.uk>
2015-04-02 12:46:04 +0100

    Basic routing

--- client/Hubble.js
+++ client/Hubble.js
@@ -1,15 +1,11 @@
-// counter starts at 0
-Session.setDefault('counter', 0);
-
-Template.hello.helpers({
+Template.list.helpers({
     counter: function () {
         return Session.get('counter');
     }
 });
 
-Template.hello.events({
-    'click button': function () {
-        // increment the counter when button is clicked
-        Session.set('counter', Session.get('counter') + 1);
+Template.list.events({
+    'click .addBranch': function () {
+        Router.go('addBranch');
     }
 });
