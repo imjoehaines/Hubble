1804099e38805f68bb1c596e175aa610647e509a
Kyle Selman <kyle@kyleselman.co.uk>
2015-04-02 14:50:02 +0100

    href links to branchView
    clicking the rows will link to branchView id o that branch

--- client/templates/list.js
+++ client/templates/list.js
@@ -1,6 +1,421 @@
 Template.list.helpers({
     branches: function () {
-        return BranchList.find();
+        return [
+    {
+        id: 124536,
+        createdOn: new Date(),
+        type: 'sprint',
+        sprints: [
+            49,
+            50
+        ],
+        crmTaskNumbers: [
+            12536,
+        ],
+        name: 'Hubble!',
+        description: 'blah blah blah',
+        team: 'Astro Fan',
+        status: 'created',
+        contributors: [],
+        tests: {
+            acceptance: {
+                done: false,
+                testers: []
+            },
+            unit: {
+                done: false,
+                testers: []
+            },
+            browser: {
+                done: false,
+                testers: []
+            }
+        },
+        review: {
+            ready: false,
+            passed: false,
+            reviewers: [
+
+            ],
+        },
+        isPassingOnCI: false,
+        mergeInfo: {
+            sucessfulMergeFromMaster: false,
+            mergedToMaster: false,
+            masterCommitId: null
+        },
+        isDeployed: false,
+        isDeprecated: false
+    },
+    {
+        id: 124536,
+        createdOn: new Date(),
+        type: 'sprint',
+        sprints: [
+            49,
+            50
+        ],
+        crmTaskNumbers: [
+            12536,
+        ],
+        name: 'Hubble!',
+        description: 'blah blah blah',
+        team: 'Astro Fan',
+        status: 'inProgress',
+        contributors: [],
+        tests: {
+            acceptance: {
+                done: false,
+                testers: []
+            },
+            unit: {
+                done: false,
+                testers: []
+            },
+            browser: {
+                done: false,
+                testers: []
+            }
+        },
+        review: {
+            ready: false,
+            passed: false,
+            reviewers: [
+
+            ],
+        },
+        isPassingOnCI: false,
+        mergeInfo: {
+            sucessfulMergeFromMaster: false,
+            mergedToMaster: false,
+            masterCommitId: null
+        },
+        isDeployed: false,
+        isDeprecated: false
+    },
+    {
+        id: 124536,
+        createdOn: new Date(),
+        type: 'sprint',
+        sprints: [
+            49,
+            50
+        ],
+        crmTaskNumbers: [
+            12536,
+        ],
+        name: 'Hubble!',
+        description: 'blah blah blah',
+        team: 'Astro Fan',
+        status: 'readyForReview',
+        contributors: [],
+        tests: {
+            acceptance: {
+                done: false,
+                testers: []
+            },
+            unit: {
+                done: false,
+                testers: []
+            },
+            browser: {
+                done: false,
+                testers: []
+            }
+        },
+        review: {
+            ready: false,
+            passed: false,
+            reviewers: [
+
+            ],
+        },
+        isPassingOnCI: false,
+        mergeInfo: {
+            sucessfulMergeFromMaster: false,
+            mergedToMaster: false,
+            masterCommitId: null
+        },
+        isDeployed: false,
+        isDeprecated: false
+    },
+    {
+        id: 124536,
+        createdOn: new Date(),
+        type: 'sprint',
+        sprints: [
+            49,
+            50
+        ],
+        crmTaskNumbers: [
+            12536,
+        ],
+        name: 'Hubble!',
+        description: 'blah blah blah',
+        team: 'Astro Fan',
+        status: 'inReview',
+        contributors: [],
+        tests: {
+            acceptance: {
+                done: false,
+                testers: []
+            },
+            unit: {
+                done: false,
+                testers: []
+            },
+            browser: {
+                done: false,
+                testers: []
+            }
+        },
+        review: {
+            ready: false,
+            passed: false,
+            reviewers: [
+
+            ],
+        },
+        isPassingOnCI: false,
+        mergeInfo: {
+            sucessfulMergeFromMaster: false,
+            mergedToMaster: false,
+            masterCommitId: null
+        },
+        isDeployed: false,
+        isDeprecated: false
+    },
+    {
+        id: 124536,
+        createdOn: new Date(),
+        type: 'sprint',
+        sprints: [
+            49,
+            50
+        ],
+        crmTaskNumbers: [
+            12536,
+        ],
+        name: 'Hubble!',
+        description: 'blah blah blah',
+        team: 'Astro Fan',
+        status: 'prepareForMaster',
+        contributors: [],
+        tests: {
+            acceptance: {
+                done: false,
+                testers: []
+            },
+            unit: {
+                done: false,
+                testers: []
+            },
+            browser: {
+                done: false,
+                testers: []
+            }
+        },
+        review: {
+            ready: false,
+            passed: false,
+            reviewers: [
+
+            ],
+        },
+        isPassingOnCI: false,
+        mergeInfo: {
+            sucessfulMergeFromMaster: false,
+            mergedToMaster: false,
+            masterCommitId: null
+        },
+        isDeployed: false,
+        isDeprecated: false
+    },
+    {
+        id: 124536,
+        createdOn: new Date(),
+        type: 'sprint',
+        sprints: [
+            49,
+            50
+        ],
+        crmTaskNumbers: [
+            12536,
+        ],
+        name: 'Hubble!',
+        description: 'blah blah blah',
+        team: 'Astro Fan',
+        status: 'readyForMaster',
+        contributors: [],
+        tests: {
+            acceptance: {
+                done: false,
+                testers: []
+            },
+            unit: {
+                done: false,
+                testers: []
+            },
+            browser: {
+                done: false,
+                testers: []
+            }
+        },
+        review: {
+            ready: false,
+            passed: false,
+            reviewers: [
+
+            ],
+        },
+        isPassingOnCI: false,
+        mergeInfo: {
+            sucessfulMergeFromMaster: false,
+            mergedToMaster: false,
+            masterCommitId: null
+        },
+        isDeployed: false,
+        isDeprecated: false
+    },
+    {
+        id: 124536,
+        createdOn: new Date(),
+        type: 'sprint',
+        sprints: [
+            49,
+            50
+        ],
+        crmTaskNumbers: [
+            12536,
+        ],
+        name: 'Hubble!',
+        description: 'blah blah blah',
+        team: 'Astro Fan',
+        status: 'mergedToMaster',
+        contributors: [],
+        tests: {
+            acceptance: {
+                done: false,
+                testers: []
+            },
+            unit: {
+                done: false,
+                testers: []
+            },
+            browser: {
+                done: false,
+                testers: []
+            }
+        },
+        review: {
+            ready: false,
+            passed: false,
+            reviewers: [
+
+            ],
+        },
+        isPassingOnCI: false,
+        mergeInfo: {
+            sucessfulMergeFromMaster: false,
+            mergedToMaster: false,
+            masterCommitId: null
+        },
+        isDeployed: false,
+        isDeprecated: false
+    },
+    {
+        id: 124536,
+        createdOn: new Date(),
+        type: 'sprint',
+        sprints: [
+            49,
+            50
+        ],
+        crmTaskNumbers: [
+            12536,
+        ],
+        name: 'Hubble!',
+        description: 'blah blah blah',
+        team: 'Astro Fan',
+        status: 'deployed',
+        contributors: [],
+        tests: {
+            acceptance: {
+                done: false,
+                testers: []
+            },
+            unit: {
+                done: false,
+                testers: []
+            },
+            browser: {
+                done: false,
+                testers: []
+            }
+        },
+        review: {
+            ready: false,
+            passed: false,
+            reviewers: [
+
+            ],
+        },
+        isPassingOnCI: false,
+        mergeInfo: {
+            sucessfulMergeFromMaster: false,
+            mergedToMaster: false,
+            masterCommitId: null
+        },
+        isDeployed: false,
+        isDeprecated: false
+    },
+    {
+        id: 124536,
+        createdOn: new Date(),
+        type: 'sprint',
+        sprints: [
+            49,
+            50
+        ],
+        crmTaskNumbers: [
+            12536,
+        ],
+        name: 'Hubble!',
+        description: 'blah blah blah',
+        team: 'Astro Fan',
+        status: 'deprecated',
+        contributors: [],
+        tests: {
+            acceptance: {
+                done: false,
+                testers: []
+            },
+            unit: {
+                done: false,
+                testers: []
+            },
+            browser: {
+                done: false,
+                testers: []
+            }
+        },
+        review: {
+            ready: false,
+            passed: false,
+            reviewers: [
+
+            ],
+        },
+        isPassingOnCI: false,
+        mergeInfo: {
+            sucessfulMergeFromMaster: false,
+            mergedToMaster: false,
+            masterCommitId: null
+        },
+        isDeployed: false,
+        isDeprecated: false
+    }
+]
     }
 });
 
