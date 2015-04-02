895ca5305a59a10bada3ac0b21d23f9e1bd2d7a9
Matt Lloyd <matthew.lloyd@mayden.co.uk>
2015-04-02 13:26:25 +0100

    Seperated out template for each branch row in the list

--- client/Hubble.js
+++ client/Hubble.js
@@ -1,6 +1,28 @@
 Template.list.helpers({
     counter: function () {
         return Session.get('counter');
+    },
+    branches: function () {
+        return [
+            {
+                name: 'Hubble',
+                teams: [
+                    {name:'AstroFan'},{name:'IPAs'},{name:'AlphaTeam'}
+                ],
+                startDate: '2015-04-02',
+                sprint: 49,
+                status: 'Created'
+            },
+            {
+                name: 'Hubble',
+                teams: [
+                    {name:'AstroFan'},{name:'IPAs'},{name:'AlphaTeam'}
+                ],
+                startDate: '2015-04-02',
+                sprint: 49,
+                status: 'Created'
+            }
+        ];
     }
 });
 
