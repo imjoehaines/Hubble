2cb2c97e3999a120c27c162434cbef70bfe73e8d
Joe Haines <hello@joehaines.co.uk>
2015-05-16 15:47:15 +0100

    Add ability to filter branches

--- client/templates/list.js
+++ client/templates/list.js
@@ -1,11 +1,24 @@
 Template.list.helpers({
     branches: function () {
-        return BranchList.find({}, {sort: {createdOn: -1}});
+        Session.setDefault('filter', 'all');
+
+        var filter = Session.get('filter');
+        var query;
+
+        if (filter === 'all') {
+            return BranchList.find({}, {sort: {createdOn: -1}});
+        }
+
+        return BranchList.find({status: {$in: [filter]}}, {sort: {createdOn: -1}});
     }
 });
 
 Template.list.events({
     'click .addBranch': function () {
         Router.go('addBranch');
+    },
+
+    'change #filterBranches': function () {
+        Session.set('filter', $('#filterBranches').val());
     }
 });
