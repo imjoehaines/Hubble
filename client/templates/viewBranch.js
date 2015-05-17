f2269259def70b343159ec82ae485163acfcb1c2
Joe Haines <hello@joehaines.co.uk>
2015-05-17 20:41:45 +0100

    rejoin getBranchFields

--- client/templates/viewBranch.js
+++ client/templates/viewBranch.js
@@ -19,39 +19,6 @@ Template.viewBranch.helpers({
     }
 });
 
-var getTestFields = function () {
-    return {
-        acceptance: {
-            done: getBooleanFromString($('#isAcceptance').val()),
-            testers: []
-        },
-        unit: {
-            done: getBooleanFromString($('#isUnit').val()),
-            testers: []
-        },
-        browser: {
-            done: getBooleanFromString($('#isBrowser').val()),
-            testers: []
-        }
-    };
-};
-
-var getReviewFields = function () {
-    return {
-        ready: getBooleanFromString($('#isReadyForReview').val()),
-        passed: getBooleanFromString($('#isReviewed').val()),
-        reviewers: $('#reviewers').val().length > 0 && $('#reviewers').val().split(', ') || [],
-    };
-};
-
-var getMergeFields = function () {
-    return {
-        sucessfulMergeFromMaster: getBooleanFromString($('#sucessfulMergeFromMaster').val()),
-        mergedToMaster: getBooleanFromString($('#mergedToMaster').val()),
-        masterCommitId: $('#masterCommitId').val(),
-    };
-};
-
 var getBranchFields = function () {
     var type = $('#type').val();
     var name = $('#name').val();
@@ -74,14 +41,34 @@ var getBranchFields = function () {
         description: description,
         team: team,
         contributors: $('#contributors').val(),
+        tests: {
+            acceptance: {
+                done: getBooleanFromString($('#isAcceptance').val()),
+                testers: []
+            },
+            unit: {
+                done: getBooleanFromString($('#isUnit').val()),
+                testers: []
+            },
+            browser: {
+                done: getBooleanFromString($('#isBrowser').val()),
+                testers: []
+            }
+        },
         isPassingOnCI: getBooleanFromString($('#isPassingOnCI').val()),
+        review: {
+            ready: getBooleanFromString($('#isReadyForReview').val()),
+            passed: getBooleanFromString($('#isReviewed').val()),
+            reviewers: $('#reviewers').val().length > 0 && $('#reviewers').val().split(', ') || [],
+        },
+        mergeInfo: {
+            sucessfulMergeFromMaster: getBooleanFromString($('#sucessfulMergeFromMaster').val()),
+            mergedToMaster: getBooleanFromString($('#mergedToMaster').val()),
+            masterCommitId: $('#masterCommitId').val(),
+        },
         isDeployed: getBooleanFromString($('#isDeployed').val()),
         isDeprecated: getBooleanFromString($('#isDeprecated').val())
     };
-
-    branch.tests = getTestFields();
-    branch.review = getReviewFields();
-    branch.mergeInfo = getMergeFields();
 
     return branch;
 };
